585429b398ed39b44e1c00df08b75633434289c0
Lightning323 <sam.p.w524@gmail.com>
2026-03-08 17:33:43 -0600

    Update SailPulley.java

--- src/main/java/com/lightning323/createkinetic/blocks/sail/SailPulley.java
+++ src/main/java/com/lightning323/createkinetic/blocks/sail/SailPulley.java
@@ -1,9 +1,17 @@
 package com.lightning323.createkinetic.blocks.sail;
 
 import com.lightning323.createkinetic.registries.KineticBlockEntities;
+import com.lightning323.createkinetic.registries.KineticItems;
+import com.simibubi.create.AllBlocks;
 import com.simibubi.create.content.contraptions.pulley.PulleyBlock;
 import com.simibubi.create.content.contraptions.pulley.PulleyBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.HitResult;
 
 public class SailPulley extends PulleyBlock {
     public SailPulley(Properties properties) {
@@ -13,5 +21,11 @@ public class SailPulley extends PulleyBlock {
     @Override
     public BlockEntityType<? extends PulleyBlockEntity> getBlockEntityType() {
         return KineticBlockEntities.ROPE_PULLEY.get();
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter world, BlockPos pos,
+                                       Player player) {
+        return KineticItems.ROPE_PULLEY.asStack();
     }
 }
