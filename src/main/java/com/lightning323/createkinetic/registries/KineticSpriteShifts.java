795b0c09c03daabbae971284ca031959455b403e
Lightning323 <sam.p.w524@gmail.com>
2026-03-17 14:02:04 -0600

    fixes

--- src/main/java/com/lightning323/createkinetic/registries/KineticSpriteShifts.java
+++ src/main/java/com/lightning323/createkinetic/registries/KineticSpriteShifts.java
@@ -8,16 +8,15 @@ import net.createmod.catnip.render.SpriteShiftEntry;
 public class KineticSpriteShifts {
 
     public static final SpriteShiftEntry
-//            SAIL_COIL = KineticSpriteShifter.get(
-//            Create.asResource("block/rope_pulley_coil"),
-//            Create.asResource("block/rope_pulley_coil_scroll"));
+            SAIL_COIL = KineticSpriteShifter.get(
+            Create.asResource("block/rope_pulley_coil"),
+            Create.asResource("block/rope_pulley_coil_scroll"));
 //    SAIL_COIL = KineticSpriteShifter.get(
 //            Create.asResource("block/blaze_burner_flame"),
 //            Create.asResource("block/blaze_burner_flame_scroll"));
-            SAIL_COIL = KineticSpriteShifter.get(
-            CreateKinetic.resource("block/sail_coil"),
-            CreateKinetic.resource("block/sail_coil_scroll"));
-
+//            SAIL_COIL = KineticSpriteShifter.get(
+//            CreateKinetic.resource("block/sail_coil_scroll"),
+//            CreateKinetic.resource("block/sail_coil_scroll"));
 
 
     public static void init() {
