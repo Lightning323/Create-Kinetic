37ffff9d6ceca3ccac71f1ba704f032f92e8d905
Lightning323 <sam.p.w524@gmail.com>
2026-04-01 18:34:04 -0600

    refactored helm control

--- src/main/java/com/lightning323/createkinetic/client/ClientForgeEvents.java
+++ src/main/java/com/lightning323/createkinetic/client/ClientForgeEvents.java
@@ -4,12 +4,14 @@ import com.lightning323.createkinetic.blocks.shipHelm.HelmControlData;
 import com.lightning323.createkinetic.blocks.shipHelm.HelmControlPacket;
 import com.lightning323.createkinetic.registries.KineticKeybindings;
 import com.lightning323.createkinetic.registries.KineticPackets;
+import com.lightning323.createkinetic.ship.KineticShipControl;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.Input;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.valkyrienskies.mod.common.entity.ShipMountingEntity;
 
 import static com.lightning323.createkinetic.CreateKinetic.MOD_ID;
 
@@ -18,6 +20,7 @@ public class ClientForgeEvents {
 
     static final HelmControlData lastControlData = new HelmControlData();
     static final HelmControlData controlData = new HelmControlData();
+    static long ticksSinceLastControl = 0;
 
     @SubscribeEvent
     public static void onClientTick(TickEvent.ClientTickEvent event) {
@@ -25,9 +28,11 @@ public class ClientForgeEvents {
             var mc = Minecraft.getInstance();
             if (mc.player == null) return;
 
-            if (KineticClientData.getControllingBlockEntity() != null) {
+            if (mc.player.getVehicle() instanceof ShipMountingEntity
+                    && KineticClientData.getControllingBlockEntity() != null) {
                 // Access the player's movement input
                 Input input = mc.player.input;
+                long time = mc.player.level().getGameTime();
 
                 float elevatorImpulse = 0;
                 if (KineticKeybindings.CONTROL_UP.isDown()) {
@@ -41,8 +46,9 @@ public class ClientForgeEvents {
                 controlData.upImpulse = elevatorImpulse;
                 controlData.sprintOn = mc.player.isSprinting();
 
-                if (!lastControlData.equals(controlData)) {
+                if (!lastControlData.equals(controlData) && time - ticksSinceLastControl > 4) {
                     lastControlData.set(controlData);
+                    ticksSinceLastControl = time;
                     KineticPackets.HELM_CONTROL.sendToServer(
                             new HelmControlPacket(KineticClientData.getControllingBlockPos(), controlData));
                 }
