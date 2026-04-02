cd0e0987139c3832c4c557d0588d35b7fd2d5e71
Lightning323 <sam.p.w524@gmail.com>
2026-04-01 20:38:47 -0600

    updates

--- src/main/java/com/lightning323/createkinetic/registries/KineticEnglishTranslationProvider.java
+++ src/main/java/com/lightning323/createkinetic/registries/KineticEnglishTranslationProvider.java
@@ -13,8 +13,8 @@ public class KineticEnglishTranslationProvider {
         provider.add("tooltip.createkinetic.enchantedballast", "Keeps your ship upright");
         provider.add("tooltip.createkinetic.anchor", "Use redstone power to anchor (freeze) your ship");
         provider.add("tooltip.createkinetic.shiptotem.freeze", "Freezes the ship after recovery");
-        provider.add("tooltip.createkinetic.frequency_filter","Matches redstone signals against their corresponding address (frequency). Used in Smart Redstone Links");
-        provider.add("tooltip.createkinetic.redstone_link","Like a redstone link but uses frequency filters instead of items to send/receive signals");
+        provider.add("tooltip.createkinetic.frequency_filter", "Matches redstone signals against their corresponding address (frequency). Used in Smart Redstone Links");
+        provider.add("tooltip.createkinetic.redstone_link", "Like a redstone link but uses frequency filters instead of items to send/receive signals");
         provider.add("createkinetic.rudder.not_enough_space", "The area must be clear in order to place a rudder blade!");
         provider.add("tooltip.createkinetic.helm", "Provides rotational power to rudders and other steering devices; Can be used alongside other controls");
         provider.add("tooltip.createkinetic.retractable_sail", "A retractable sail, useful for propelling a ship");
@@ -22,5 +22,8 @@ public class KineticEnglishTranslationProvider {
         provider.add("tooltip.createkinetic.rudder", "Can be used to steer or dive a ship");
         provider.add("tooltip.createkinetic.forward_crank", "A crank that can be used to propel a ship");
         provider.add("tooltip.createkinetic.elevator_crank", "A crank that can be used to propel a ship");
+        provider.add("key.createkinetic.control", "Kinetic Controls");
+        provider.add("key.createkinetic.control.down", "Elevator (Down)");
+        provider.add("key.createkinetic.control.up", "Elevator (Up)");
     }
 }
