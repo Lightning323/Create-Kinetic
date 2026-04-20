03dd09523e78e57d865b2ad3bfc0e164125c4b98
Lightning323 <sam.p.w524@gmail.com>
2026-04-20 17:29:04 -0600

    changed name

--- src/main/java/org/lightning323/create_kinetic/Config.java
+++ src/main/java/org/lightning323/create_kinetic/Config.java
@@ -1,4 +1,4 @@
-package org.lightning323.creative_mode_tweaks;
+package org.lightning323.create_kinetic;
 
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Neo's config APIs
-@EventBusSubscriber(modid = Creative_mode_tweaks.MODID, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = CreateKinetic.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
--- src/main/java/org/lightning323/create_kinetic/CreateKinetic.java
+++ src/main/java/org/lightning323/create_kinetic/CreateKinetic.java
@@ -1,4 +1,4 @@
-package org.lightning323.creative_mode_tweaks;
+package org.lightning323.create_kinetic;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -33,35 +33,35 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(Creative_mode_tweaks.MODID)
-public class Creative_mode_tweaks {
+@Mod(CreateKinetic.MODID)
+public class CreateKinetic {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "creative_mode_tweaks";
+    public static final String MODID = "create_kinetic";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "creative_mode_tweaks" namespace
+    // Create a Deferred Register to hold Blocks which will all be registered under the "create_kinetic" namespace
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "creative_mode_tweaks" namespace
+    // Create a Deferred Register to hold Items which will all be registered under the "create_kinetic" namespace
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "creative_mode_tweaks" namespace
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "create_kinetic" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // Creates a new Block with the id "creative_mode_tweaks:example_block", combining the namespace and path
+    // Creates a new Block with the id "create_kinetic:example_block", combining the namespace and path
     public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
-    // Creates a new BlockItem with the id "creative_mode_tweaks:example_block", combining the namespace and path
+    // Creates a new BlockItem with the id "create_kinetic:example_block", combining the namespace and path
     public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
 
-    // Creates a new food item with the id "creative_mode_tweaks:example_id", nutrition 1 and saturation 2
+    // Creates a new food item with the id "create_kinetic:example_id", nutrition 1 and saturation 2
     public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder().alwaysEdible().nutrition(1).saturationModifier(2f).build()));
 
-    // Creates a creative tab with the id "creative_mode_tweaks:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.creative_mode_tweaks")).withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> EXAMPLE_ITEM.get().getDefaultInstance()).displayItems((parameters, output) -> {
+    // Creates a creative tab with the id "create_kinetic:example_tab" for the example item, that is placed after the combat tab
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.create_kinetic")).withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> EXAMPLE_ITEM.get().getDefaultInstance()).displayItems((parameters, output) -> {
         output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
     }).build());
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public Creative_mode_tweaks(IEventBus modEventBus, ModContainer modContainer) {
+    public CreateKinetic(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -73,7 +73,7 @@ public class Creative_mode_tweaks {
         CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (Creative_mode_tweaks) to respond directly to events.
+        // Note that this is necessary if and only if we want *this* class (create_kinetic) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
