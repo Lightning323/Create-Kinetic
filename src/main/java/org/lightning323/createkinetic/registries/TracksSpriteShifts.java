c1eb41e8afb62109b3e2be86c71b7a33ca0d9fa8
Lightning323 <sam.p.w524@gmail.com>
2026-08-13 16:47:56 -0600

    added dye depot support for tracks

--- src/main/java/org/lightning323/createkinetic/registries/TracksSpriteShifts.java
+++ src/main/java/org/lightning323/createkinetic/registries/TracksSpriteShifts.java
@@ -30,16 +30,61 @@ public class TracksSpriteShifts {
         return SpriteShifter.get((ResourceLocation) ResourceLocation.tryBuild((String) CreateKinetic.ID, (String) originalLocation), (ResourceLocation) ResourceLocation.tryBuild((String) CreateKinetic.ID, (String) targetLocation));
     }
 
-    private static String textureName(DyeColor color) {
-        return color == DyeColor.LIME ? "light_green" : color.getName();
-    }
-
     public static void init() {
     }
 
     static {
         for (DyeColor color : DyeColor.values()) {
-            String colorName = TracksSpriteShifts.textureName(color);
+//            System.out.println("DYE COLOR ======================== "+color);
+            /**
+             * DYE COLOR ======================== white
+             * DYE COLOR ======================== orange
+             * DYE COLOR ======================== magenta
+             * DYE COLOR ======================== light_blue
+             * DYE COLOR ======================== yellow
+             * DYE COLOR ======================== lime
+             * DYE COLOR ======================== pink
+             * DYE COLOR ======================== gray
+             * DYE COLOR ======================== light_gray
+             * DYE COLOR ======================== cyan
+             * DYE COLOR ======================== purple
+             * DYE COLOR ======================== blue
+             * DYE COLOR ======================== brown
+             * DYE COLOR ======================== green
+             * DYE COLOR ======================== red
+             * DYE COLOR ======================== black
+             * DYE COLOR ======================== maroon
+             * DYE COLOR ======================== rose
+             * DYE COLOR ======================== coral
+             * DYE COLOR ======================== indigo
+             * DYE COLOR ======================== navy
+             * DYE COLOR ======================== slate
+             * DYE COLOR ======================== olive
+             * DYE COLOR ======================== amber
+             * DYE COLOR ======================== beige
+             * DYE COLOR ======================== teal
+             * DYE COLOR ======================== mint
+             * DYE COLOR ======================== aqua
+             * DYE COLOR ======================== verdant
+             * DYE COLOR ======================== forest
+             * DYE COLOR ======================== ginger
+             * DYE COLOR ======================== tan
+             */
+            //TODO: I should have unique colors for every dye depot color, but... I don't want to
+            String colorName = color.getName();
+            colorName = switch (colorName) {
+                case "maroon" -> "red";
+                case "olive" -> "green";
+                case "beige","tan","amber" -> "yellow";
+                case "teal", "aqua", "mint" -> "cyan";
+                case "forest", "verdant" -> "green";
+                case "ginger" -> "orange";
+                case "coral" -> "pink";
+                case "indigo", "navy" -> "blue";
+                case "slate" -> "gray";
+                case "rose" -> "magenta";
+                default -> colorName;
+            };
             COLORED_BELTS.put(color, TracksSpriteShifts.get("block/belt", "block/belt_" + colorName + "_scroll"));
         }
     }
